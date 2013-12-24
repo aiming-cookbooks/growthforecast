4c281fe37874aa2abb7cf9e045f4075c71c816b1
futoase <futoase@gmail.com>
2013-12-24 19:41:42 +0900

    Created: Support test-kitchen.

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org/'
+
+group :development do
+  gem 'test-kitchen'
+  gem 'berkshelf'
+  gem 'serverspec'
+  gem 'knife-solo'
+end
+gem 'kitchen-vagrant'
--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,9 @@ description      'Installs/Configures growthforecast'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends          'perlbrew'
+depends          'yum'
 depends          'mysql'
 depends          'build-essential'
+depends          'perlbrew'
 
 supports         'centos'
